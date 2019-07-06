1bdcd5d228d3822d535e08771461d22d27946e55
Kirill Fomichev <fanatid@ya.ru>
2019-07-06 10:38:29 +0300

    update tests

--- test/parse-text.js
+++ test/parse-text.js
@@ -1,19 +1,18 @@
 const fs = require('fs')
+const yaml = require('js-yaml')
 const path = require('path')
 const test = require('tape')
 const jsyesql = require('../')
-const fixtures = require('./parse-text-fixtures')
+
+const fixtures = yaml.safeLoad(fs.readFileSync(path.join(__dirname, 'parse-text-fixtures.yml')))
 
 for (const fixture of fixtures) {
-  test(`fixture#${fixture.name}`, (t) => {
-    const location = path.join(__dirname, `${path.parse(__filename).name}-fixtures-${fixture.name}`)
-    const fixtureText = fs.readFileSync(location, 'utf-8')
+  test(`parse-text-fixture#${fixture.name}`, (t) => {
+    const arr = jsyesql.parseTextToArray(fixture.text)
+    t.same(arr, fixture.queries)
 
-    const arr = jsyesql.parseTextToArray(fixtureText)
-    t.same(arr, fixture.queries.array)
-
-    const obj = jsyesql.parseTextToObject(fixtureText)
-    t.same(obj, fixture.queries.obj)
+    const obj = jsyesql.parseTextToObject(fixture.text)
+    t.same(obj, jsyesql.arrayToObject(fixture.queries))
 
     t.end()
   })
